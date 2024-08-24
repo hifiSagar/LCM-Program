e02aa946fdf1abfb8967667ef14079c7808e910f
SAGAR T <ssagu6661@gmail.com>
2024-08-24 09:52:46 -0400

    Add files via upload

---
+++ c19.py
@@ -0,0 +1,14 @@
+#LCM IN PYTHON
+
+x= int(input("x>> "))
+y= int(input("y>> "))
+if x>y:
+    greater= x
+else:
+    greater=y
+    while(True):
+        if((greater%x==0)&(greater%y==0) ):
+            lcm=greater
+            break
+        greater += 1
+print(f"LCM of {x} and {y} is: {lcm}")
